53179e51a20c3cb7746e5bcdbc39d78800a16af0
Andreas Schmidt <moepmoep12@gmail.com>
2022-07-28 16:25:34 +0200

    Add nyc for code coverage

--- .mocharc.js
+++ .mocharc.js
@@ -1,8 +1,7 @@
-const testPath = process.env.TEST_PATH || "tests/**/[!Errors]*.ts";
+const testPath = process.env.TEST_PATH || "tests/**/*.ts";
 
 module.exports = {
   spec: [testPath],
-  exclude: process.env.TEST_ERRORS ? "" : ".*Errors.test.ts",
   require: ["ts-node/register", "./tests/mochaFixtures.ts"],
   recursive: true,
   extension: [
